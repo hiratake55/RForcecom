c1fc6b06e29ee3413bab30b8a1c38fee2c15c1fe
Takekatsu Hiramura <hiratake55@gmail.com>
2012-05-22 00:47:45 +0900

    'added'

---
+++ RForcecom-0.2/RForcecom/R/rforcecom.delete.R
@@ -0,0 +1,30 @@
+rforcecom.delete <-
+function(session, objectName, id){
+ # Send records
+ h <- basicHeaderGatherer()
+ t <- basicTextGatherer()
+ endpointPath <- rforcecom.api.getRecordEndpoint(session['apiVersion'], objectName, id)
+ URL <- paste(session['instanceURL'], endpointPath, sep="")
+ OAuthString <- paste("OAuth", session['sessionID'])
+ httpHeader <- c("Authorization"=OAuthString, "Accept"="application/xml", 'Content-Type'="application/xml")
+ httpResponse <- curlPerform(url=URL, httpheader=httpHeader, headerfunction = h$update, writefunction = t$update, ssl.verifypeer=F, customrequest="DELETE")
+ 
+ # BEGIN DEBUG
+ if(exists("rforcecom.debug") && rforcecom.debug){ message(URL) }
+ if(exists("rforcecom.debug") && rforcecom.debug){ message(t$value()) }
+ # END DEBUG
+  
+ # Parse XML
+ if(t$value() != ""){
+  x.root <- xmlRoot(xmlTreeParse(t$value(), asText=T))
+  
+  # Check whether it success
+  try(errorcode <- iconv(xmlValue(x.root[['Error']][['errorCode']]), from="UTF-8", to=""), TRUE)
+  try(errormessage <- iconv(xmlValue(x.root[['Error']][['message']]), from="UTF-8", to=""), TRUE)
+  if(errorcode != "NA" && errormessage != "NA"){
+   stop(paste(errorcode, errormessage, sep=": "))
+  }
+ }
+ 
+}
+
---
+++ RForcecom-0.2/RForcecom/R/rforcecom.getServerTimestamp.R
@@ -0,0 +1,29 @@
+rforcecom.getServerTimestamp <-
+function(session){
+ # Send records
+ h <- basicHeaderGatherer()
+ t <- basicTextGatherer()
+ endpointPath <- rforcecom.api.getRestEndpoint(session['apiVersion'])
+ URL <- paste(session['instanceURL'], endpointPath, "/", sep="")
+ OAuthString <- paste("OAuth", session['sessionID'])
+ httpHeader <- c("Authorization"=OAuthString, "Accept"="application/xml", "X-PrettyPrint"="1")
+ curlPerform(url=URL, httpheader=httpHeader, headerfunction = h$update, writefunction = t$update, ssl.verifypeer=F)
+ 
+ # BEGIN DEBUG
+ if(exists("rforcecom.debug") && rforcecom.debug){ message(URL) }
+ if(exists("rforcecom.debug") && rforcecom.debug){ message(h$value()) }
+ if(exists("rforcecom.debug") && rforcecom.debug){ message(t$value()) }
+ # END DEBUG
+ 
+ # Format as a date
+ timeval <- unname(h$value()['Date'])
+ tzval <- substr(timeval, nchar(timeval)-2, nchar(timeval))
+ localtimeval <- substr(timeval, 0, nchar(timeval)-4)
+ lct <- Sys.getlocale("LC_TIME")
+ Sys.setlocale("LC_TIME", "C")
+ posixtime <- as.POSIXlt(localtimeval, format="%a, %d %b %Y %X", tz=tzval)
+ Sys.setlocale("LC_TIME", lct)
+ 
+ return(posixtime)
+}
+
