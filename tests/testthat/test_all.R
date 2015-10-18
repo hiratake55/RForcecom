84373e696c4c8817c35984b1ba705520cb5ae7fe
Steven Mortimer <reportmort@gmail.com>
2015-10-18 14:22:28 -0400

    Change Travis Config
    The prior configuration was failing for a reason of environment variables not being shared, not because it was wrong, so bringing that version back.

--- tests/testthat/test_all.R
+++ tests/testthat/test_all.R
@@ -5,8 +5,8 @@ context("Testing login feature")
 test_that("Checking login has sucess", {
   username <- Sys.getenv("RFORCECOM_EMAIL")
   password <- Sys.getenv("RFORCECOM_PASSWORD")
-  instanceURL <- "https://na14.salesforce.com/"
-  apiVersion <- "27.0"
+  instanceURL <- Sys.getenv("RFORCECOM_INSTANCEURL")
+  apiVersion <- "34.0" 
   session <- NULL
   tryCatch(session <- rforcecom.login(username, password, instanceURL, apiVersion))
   expect_true(length(session) > 0)
