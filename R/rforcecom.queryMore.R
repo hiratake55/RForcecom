c1c265c8c824059f9fab65bea4e5e1413572c9b7
Steven Mortimer <reportmort@gmail.com>
2015-10-24 15:43:33 -0400

    Refactor rforcecom.query function
    Change the internal behavior of rforcecom.query to handle nested queries, obey global options setting for stringsAsFactors, and increase speed.

--- R/rforcecom.queryMore.R
+++ R/rforcecom.queryMore.R
@@ -20,7 +20,7 @@ function(session, nextRecordsUrl){
  # END DEBUG
  
  # Parse XML
- x.root <- xmlRoot(xmlTreeParse(t$value(), asText=T))
+ x.root <- xmlRoot(xmlParse(t$value(), asText=T))
  
  # Check whether it success or not
  errorcode <- NA
@@ -28,41 +28,18 @@ function(session, nextRecordsUrl){
  try(errorcode <- iconv(xmlValue(x.root[['Error']][['errorCode']]), from="UTF-8", to=""), TRUE)
  try(errormessage <- iconv(xmlValue(x.root[['Error']][['message']]), from="UTF-8", to=""), TRUE)
  if(!is.na(errorcode) && !is.na(errormessage)){
-  stop(paste(errorcode, errormessage, sep=": "))
+   stop(paste(errorcode, errormessage, sep=": "))
  }
  
- # Convert XML to data frame
- xns <- getNodeSet(xmlParse(t$value()),'//records')
- xls <- lapply(lapply(xns, xmlToList), unlist)
- xls.rows <- length(xls)
- xls.colnames <- unique(unlist(lapply(xls, names)))
- xls.cols <- length(xls.colnames)
- xdf <- as.data.frame(replicate(xls.cols, rep(as.character(NA), xls.rows), simplify = FALSE), stringsAsFactors = FALSE)
- names(xdf) <- xls.colnames
- 
- # When it is a empty data set
- if(nrow(xdf) == 0){ return(xdf) }
- 
- # Fill data from retrieved XML
- for(i in 1:length(xls)){
-  xdf[i, names(xls[[i]])] <- t(xls[[i]])
- }
- # Remove field attributes
- xdf <- subset(xdf, select=!grepl('\\.attrs\\.', names(xdf)))
- 
- # Convert charset from UTF-8
- xdf.iconv <- data.frame(lapply(xdf, iconv, from="UTF-8", to=""), stringsAsFactors=FALSE)
- 
- # Convert strings to correct data types
- xdf.iconv <- lapply(xdf.iconv, type.convert)
+ resultset <- query_parser(x.root)
  
  # Check whether it has next record
  try(nextRecordsUrl <- iconv(xmlValue(x.root[['nextRecordsUrl']]), from="UTF-8", to=""), TRUE)
  if(!is.na(nextRecordsUrl)){
-  nextRecords <- rforcecom.queryMore(session, nextRecordsUrl)
-  xdf.iconv <- rbind.fill(xdf.iconv,nextRecords)
+   nextRecords <- rforcecom.queryMore(session, nextRecordsUrl)
+   resultset <- rbind.fill(resultset, nextRecords)
  }
  
- return(data.frame(xdf.iconv))
+ return(resultset)
 }
 
