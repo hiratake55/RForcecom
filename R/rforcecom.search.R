cae26986d995814b89be787060e36c2a042d9b97
Steven Mortimer <reportmort@gmail.com>
2015-12-21 11:42:20 -0500

    Expand Test Coverage
    Add unit tests for the REST API, Bulk API, and utilities of the package. The utilities are technically REST API calls, but do not involve changes in Salesforce records, so we split the testing scripts.
    
    Fix rforcecom.search from erroring when there is a malformed search string. The new version handles malformed search strings and also returns a NULL object whenever no search results are found, which is more intuitive than a 1 row 0 column data.frame

--- R/rforcecom.search.R
+++ R/rforcecom.search.R
@@ -17,23 +17,24 @@ function(session, queryString){
  if(exists("rforcecom.debug") && rforcecom.debug){ message(t$value()) }
  # END DEBUG
  
+ x.root <- xmlRoot(xmlTreeParse(t$value(), asText=T))
+ 
+ # Check whether it success or not
+ errorcode <- NA
+ errormessage <- NA
+ try(errorcode <- iconv(xmlValue(x.root[['Error']][['errorCode']]), from="UTF-8", to=""), TRUE)
+ try(errormessage <- iconv(xmlValue(x.root[['Error']][['message']]), from="UTF-8", to=""), TRUE)
+ if(!anyNA(errorcode,errormessage)){
+   stop(paste(errorcode, errormessage, sep=": "))
+ }
+ 
  # Parse XML
  xns <- getNodeSet(xmlParse(t$value()),'//SearchResults/SearchResult')
  xls <- sapply(xns, function(xn){ list(Id=xmlValue(xn), type=xmlGetAttr(xn, "type"), url=xmlGetAttr(xn, "url")) })
- xdf <- data.frame(t(xls))
- 
- # Parse XML
- if(t$value() != ""){
-  x.root <- xmlRoot(xmlTreeParse(t$value(), asText=T))
-  
-  # Check whether it success or not
-  errorcode <- NA
-  errormessage <- NA
-  try(errorcode <- iconv(xmlValue(x.root[['Error']][['errorCode']]), from="UTF-8", to=""), TRUE)
-  try(errormessage <- iconv(xmlValue(x.root[['Error']][['message']]), from="UTF-8", to=""), TRUE)
-  if(!is.na(errorcode) && !is.na(errormessage)){
-   stop(paste(errorcode, errormessage, sep=": "))
-  }
+ if(length(xls)>0){
+   xdf <- data.frame(t(xls))
+ } else {
+   xdf <- NULL
  }
  
  return(xdf)
